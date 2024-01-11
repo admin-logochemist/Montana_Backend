9d5275819d2c01059a4403d85effd530a52026ef
Umar <umar.techsmiths@gmail.com>
2024-01-11 22:58:51 +0500

    1/11/2024

--- src/index.js
+++ src/index.js
@@ -1,40 +1,38 @@
-const { default: axios } = require('axios');
-const express = require('express');
+const { default: axios } = require("axios");
+const express = require("express");
 const app = express();
-const path = require('path')
+const path = require("path");
 const PORT = process.env.PORT || 3000;
-const ftp = require("basic-ftp") 
+const ftp = require("basic-ftp");
 
 app.use(express.json());
 // Define routes
 app.get("/", (req, res) => {
-  res.send("Hello, World!");
+  res.send(`Hello, World! deployed on ${new Date().toLocaleDateString()}`);
 });
 // Route 2: About
 app.get("/about", (req, res) => {
   res.send("This is the about page.");
-})
-app.get('/check',async (req, res) => {
-  const client = new ftp.Client()
-  client.ftp.verbose = true
-
-      await client.access({
-          host: "ftps.rsrgroup.com",
-          user: "62156",
-          password: "KU3MTcd9",
-          port:2222,
-          secure: true
-      })
-      console.log("=====>",await client.list())
-      const response = await client.downloadToDir("upload/images", "/ftp_images/")
-      ;
-
-  res.send('Hello, World!');
 });
-// Route 2: About 
-app.get('/api/items', async(req, res) => {
-  let response; 
-   const payload = {
+app.get("/check", async (req, res) => {
+  const client = new ftp.Client();
+  client.ftp.verbose = true;
+
+  await client.access({
+    host: "ftps.rsrgroup.com",
+    user: "62156",
+    password: "KU3MTcd9",
+    port: 2222,
+    secure: true,
+  });
+  console.log("=====>", await client.list());
+  const response = await client.downloadToDir("upload/images", "/ftp_images/");
+  res.send("Hello, World!");
+});
+// Route 2: About
+app.get("/api/items", async (req, res) => {
+  let response;
+  const payload = {
     Username: 99901,
     Password: "webuser1",
     POS: "I",
@@ -42,28 +40,29 @@ app.get('/api/items', async(req, res) => {
     Limit: 1,
     // Offset: (currentPage - 1) * itemsPerPage
   };
-  const apiEndpoint="https://test.rsrgroup.com/api/rsrbridge/1.0/pos/get-items"
+  const apiEndpoint =
+    "https://test.rsrgroup.com/api/rsrbridge/1.0/pos/get-items";
 
-await  axios.post(apiEndpoint,payload, {
-  headers: { 'Content-Type': 'application/json' },
-  })
-    .then(res => {
-      // Handle the response data
-      response=res.data.Items
-      console.log('API Response:', res,"====>",res.data.TotalFound);
+  await axios
+    .post(apiEndpoint, payload, {
+      headers: { "Content-Type": "application/json" },
     })
-    .catch(error => {
+    .then((res) => {
+      // Handle the response data
+      response = res.data.Items;
+      console.log("API Response:", res, "====>", res.data.TotalFound);
+    })
+    .catch((error) => {
       // Handle errors
-      console.error('Error:', error.message);
+      console.error("Error:", error.message);
     });
-    res.status(200).send({mydata:response})
+  res.status(200).send({ mydata: response });
 });
 
 // Route 3: Contact
-app.get('/contact', (req, res) => {
-  res.send('Contact us at contact@example.com.');
+app.get("/contact", (req, res) => {
+  res.send("Contact us at contact@example.com.");
 });
-
 
 app.get("/items", async (req, res) => {
   try {
@@ -76,11 +75,11 @@ app.get("/items", async (req, res) => {
         SortBy: "available-quantity",
       }
     );
-    console.log("response =>",response);
+    console.log("response =>", response);
     return res.status(200).send({
       error: false,
       message: "Got All Items",
-      data:response.data  
+      data: response.data,
     });
   } catch (error) {
     return res.status(500).send({
@@ -93,4 +92,4 @@ app.get("/items", async (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
-module.exports = app
+module.exports = app;
