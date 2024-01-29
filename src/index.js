f92a53eaf9d3f6ac120b4e44887bdcef015c495b
ashartechsmith <ashar.techsmiths@gmail.com>
2024-01-29 13:58:15 -0800

    updateapipostasdasd

--- src/index.js
+++ src/index.js
@@ -45,7 +45,7 @@ app.post("/api/items/:id", async (req, res) => {
     Username: 99901,
     Password: "webuser1",
     POS: "I",
-    Limit: 3500,
+    Limit: "3500",
     WithAttributes: true,
     Departments: req.params.id
     // Offset: (currentPage - 1) * itemsPerPage
